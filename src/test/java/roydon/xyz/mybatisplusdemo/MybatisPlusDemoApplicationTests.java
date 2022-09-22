704bce0c1c914a6b347d22f7580dc1029bfdfa2b
roydonGuo <3133010060@qq.com>
2022-09-22 21:08:20 +0800

    test in here

--- src/test/java/roydon/xyz/mybatisplusdemo/MybatisPlusDemoApplicationTests.java
+++ src/test/java/roydon/xyz/mybatisplusdemo/MybatisPlusDemoApplicationTests.java
@@ -23,88 +23,81 @@ class MybatisPlusDemoApplicationTests {
 
 
     /**
-     * 搜索
+     * select---------------------------------------------------------------
      */
-
-
-    /**
-     * 修改
-     */
-
-
-    /**
-     * 增加
-     */
-
-
-    /**
-     * 删除
-     */
-
-
-    
-    @Test
-    void contextLoads() {
-        List<User> userList = userMapper.selectBatchIds(Arrays.asList("1L", "2L", "3L"));
-        for (User user : userList) {
-            System.out.println(user);
-        }
-    }
-
-    @Test
-    void selPage() {
-
-        IPage<User> page = new Page<>(0,2);
-
-        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-        queryWrapper.like("user_name","zh");
-
-        userMapper.selectPage(page,queryWrapper);
-
-        System.out.println("数据总条数"+page.getTotal());
-        System.out.println("数据总页数"+page.getPages());
-        System.out.println("当前页数"+page.getCurrent()); // getRecords为数据list
-
-
-    }
-    @Test
-    void selById(){
-
-        userMapper.getById(1L);
-    }
-
-    @Test
-    public void testInsert() {
-        User user = new User();
-        user.setEmail("2@itcast.cn");
-        user.setAge(301);
-        user.setUserName("caocao1");
-        user.setNickName("曹操1");
-        user.setPassword("123456");
-        user.setAddress("北京");
-
-        int result = this.userMapper.insert(user); //result数据库受影响的行数
-        System.out.println("result => " + result);
-
-        //获取自增长后的id值, 自增长后的id值会回填到user对象中
-        System.out.println("id => " + user.getId());
-    }
-
     @Test
     public void testSelectById() {
-        User user = this.userMapper.selectById(2L);
-        System.out.println(user);
+        // 根据 ID 查询
+        userMapper.selectById(2L);
     }
 
     @Test
+    public void testSelectOne() {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("name", "张三"); //查询条件
+        // 根据 entity 条件，查询一条记录。查询的数据超过一条时，会抛出异常(例如：wrapper.eq("password", "123456");)
+        userMapper.selectOne(wrapper);
+    }
+
+    @Test
+    void testSelectBatchIds() {
+        // 查询（根据ID 批量查询），返回 List<User> 集合，若ID 不存在像100L，那么只会查出存在的ID
+        userMapper.selectBatchIds(Arrays.asList(1L, 2L, 3L));
+//        userMapper.selectBatchIds(Arrays.asList(1L, 2L, 3L,100L)); // 只会查出1，2，3
+    }
+
+    @Test
+    public void testSelectList() {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        //设置查询条件
+        wrapper.like("email", "itcast");
+        userMapper.selectList(wrapper);
+    }
+
+    @Test
+    public void testSelectByMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("user_name", "zhangsan");
+        map.put("password", "123456");
+        // 查询（根据 columnMap 条件）
+        userMapper.selectByMap(map);
+    }
+
+    @Test
+    public void testSelectPage() {
+
+        IPage<User> page = new Page<>(0, 2); // 第一页，两条数据
+
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("password", "123456");
+
+        userMapper.selectPage(page, queryWrapper);
+
+        System.out.println("数据总条数" + page.getTotal());
+        System.out.println("数据总页数" + page.getPages());
+        System.out.println("当前页数" + page.getCurrent()); // getRecords为数据list
+    }
+
+    @Test
+    public void testSelectCount() {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.gt("age", 20); // 条件：年龄大于20岁的用户
+        // 根据 Wrapper 条件，查询总记录数，返回整数类型
+        userMapper.selectCount(wrapper);
+    }
+
+    /**
+     * update---------------------------------------------------------
+     */
+    @Test
     public void testUpdateById() {
+        // 根据 ID 修改，返回整型，表示影响的行数
         User user = new User();
         user.setId(1L); //条件，根据id更新
         user.setAge(19); //更新的字段
         user.setPassword("666666");
 
-        int result = this.userMapper.updateById(user);
-        System.out.println("result => " + result);
+        userMapper.updateById(user);
     }
 
     @Test
@@ -114,133 +107,87 @@ class MybatisPlusDemoApplicationTests {
         user.setPassword("8888888");
 
         QueryWrapper<User> wrapper = new QueryWrapper<>();
-        wrapper.eq("user_name", "zhangsan"); //匹配user_name = zhangsan 的用户数据
+        wrapper.eq("user_name", "zhangsan"); //匹配 user_name = zhangsan 的用户数据
 
-        //根据条件做更新
-        int result = this.userMapper.update(user, wrapper);
-        System.out.println("result => " + result);
+        // 根据 whereWrapper 条件，更新记录
+        userMapper.update(user, wrapper);
     }
 
     @Test
     public void testUpdate2() {
-
-        UpdateWrapper<User> wrapper = new UpdateWrapper<>();
-        wrapper.set("age", 21).set("password", "999999") //更新的字段
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.set("age", 21).set("password", "999999") //更新的字段
                 .eq("user_name", "zhangsan"); //更新的条件
-
-        //根据条件做更新
-        int result = this.userMapper.update(null, wrapper);
-        System.out.println("result => " + result);
+        userMapper.update(null, updateWrapper);
     }
 
+    /**
+     * insert
+     */
     @Test
-    public void testDeleteById(){
-        // 根据id删除数据
-        int result = this.userMapper.deleteById(9L);
-        System.out.println("result => " + result);
+    public void testInsert() {
+        User user = new User();
+        user.setUserName("baomidou");
+        user.setNickName("苞米豆");
+        user.setPassword("123456");
+        user.setAge(30);
+        user.setEmail("insert@itcast.cn");
+        user.setAddress("北京");
+        userMapper.insert(user); //result数据库受影响的行数
+        //获取自增长后的id值, 自增长后的id值会回填到user对象中
+        System.out.println("id => " + user.getId());
     }
 
+    /**
+     * delete
+     */
     @Test
-    public void testDeleteByMap(){
-
-        Map<String,Object> map = new HashMap<>();
-        map.put("user_name", "zhangsan");
-        map.put("password", "999999");
-
-        // 根据map删除数据，多条件之间是and关系
-        int result = this.userMapper.deleteByMap(map);
-        System.out.println("result => " + result);
-    }
-
-    @Test
-    public void testDelete(){
+    public void testDelete() {
 
         //用法一：
 //        QueryWrapper<User> wrapper = new QueryWrapper<>();
-//        wrapper.eq("user_name", "caocao1")
+//        wrapper.eq("user_name", "baomidou")
 //                .eq("password", "123456");
 
         //用法二：
         User user = new User();
+        user.setUserName("baomidou");
         user.setPassword("123456");
-        user.setUserName("caocao");
 
         QueryWrapper<User> wrapper = new QueryWrapper<>(user);
-
         // 根据包装条件做删除
-        int result = this.userMapper.delete(wrapper);
-        System.out.println("result => " + result);
+        userMapper.delete(wrapper);
     }
 
     @Test
-    public void  testDeleteBatchIds(){
+    public void testDeleteById() {
+        // 根据id删除数据
+        userMapper.deleteById(5L);
+    }
+
+    @Test
+    public void testDeleteByMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("user_name", "lisi");
+        map.put("password", "123456");
+
+        // 根据map删除数据，多条件之间是and关系
+        userMapper.deleteByMap(map);
+    }
+
+    @Test
+    public void testDeleteBatchIds() {
         // 根据id批量删除数据
-        int result = this.userMapper.deleteBatchIds(Arrays.asList(10L, 11L));
-        System.out.println("result => " + result);
+        userMapper.deleteBatchIds(Arrays.asList(3L, 4L));
+//        userMapper.deleteBatchIds(Arrays.asList(3L, 4L,100L));
     }
 
+    /**
+     * 自定义方法
+     */
     @Test
-    public void testSelectBatchIds(){
-        // 根据id批量查询数据
-        List<User> users = this.userMapper.selectBatchIds(Arrays.asList(2L, 3L, 4L, 100L));
-        for (User user : users) {
-            System.out.println(user);
-        }
-    }
-
-    @Test
-    public void testSelectOne(){
-        QueryWrapper<User> wrapper = new QueryWrapper<>();
-        //查询条件
-        wrapper.eq("password", "123456");
-        // 查询的数据超过一条时，会抛出异常
-        User user = this.userMapper.selectOne(wrapper);
-        System.out.println(user);
-    }
-
-    @Test
-    public void testSelectCount(){
-
-        QueryWrapper<User> wrapper = new QueryWrapper<>();
-        wrapper.gt("age", 20); // 条件：年龄大于20岁的用户
-
-        // 根据条件查询数据条数
-        Integer count = this.userMapper.selectCount(wrapper);
-        System.out.println("count => " + count);
-    }
-
-    @Test
-    public void testSelectList(){
-        QueryWrapper<User> wrapper = new QueryWrapper<>();
-        //设置查询条件
-        wrapper.like("email", "itcast");
-
-        List<User> users = this.userMapper.selectList(wrapper);
-        for (User user : users) {
-            System.out.println(user);
-        }
-    }
-
-    // 测试分页查询
-    @Test
-    public void testSelectPage(){
-
-        Page<User> page = new Page<>(3,1); //查询第一页，查询1条数据
-
-        QueryWrapper<User> wrapper = new QueryWrapper<>();
-        //设置查询条件
-        wrapper.like("email", "itcast");
-
-        IPage<User> iPage = this.userMapper.selectPage(page, wrapper);
-        System.out.println("数据总条数： " + iPage.getTotal());
-        System.out.println("数据总页数： " + iPage.getPages());
-        System.out.println("当前页数： " + iPage.getCurrent());
-
-        List<User> records = iPage.getRecords();
-        for (User record : records) {
-            System.out.println(record);
-        }
-
+    void selById() {
+        userMapper.getById(1L);
     }
 
     /**
@@ -251,11 +198,10 @@ class MybatisPlusDemoApplicationTests {
 //        User user = this.userMapper.findById(2L);
 //        System.out.println(user);
 //    }
-
     @Test
-    public void testAllEq(){
+    public void testAllEq() {
 
-        Map<String,Object> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
         params.put("name", "李四");
         params.put("age", "20");
         params.put("password", null);
@@ -269,18 +215,13 @@ class MybatisPlusDemoApplicationTests {
         //SELECT id,user_name,name,age,email AS mail FROM tb_user WHERE age = ?
 //        wrapper.allEq((k, v) -> (k.equals("age") || k.equals("id")) , params);
         //SELECT id,user_name,name,age,email AS mail FROM tb_user WHERE name = ? AND age = ?
-        wrapper.allEq((k, v) -> (k.equals("age") || k.equals("id") || k.equals("name")) , params);
-
-        List<User> users = this.userMapper.selectList(wrapper);
-        for (User user : users) {
-            System.out.println(user);
-        }
+        wrapper.allEq((k, v) -> (k.equals("age") || k.equals("id") || k.equals("name")), params);
+        userMapper.selectList(wrapper);
     }
 
     @Test
     public void testEq() {
         QueryWrapper<User> wrapper = new QueryWrapper<>();
-
         //SELECT id,user_name,password,name,age,email FROM tb_user WHERE password = ? AND age >= ? AND name IN (?,?,?)
         wrapper.eq("password", "123456")
                 .ge("age", 20)
@@ -294,33 +235,26 @@ class MybatisPlusDemoApplicationTests {
     }
 
     @Test
-    public void testLike(){
+    public void testLike() {
         QueryWrapper<User> wrapper = new QueryWrapper<>();
         // SELECT id,user_name,name,age,email AS mail FROM tb_user WHERE name LIKE ?
         // 参数：%五(String)
         wrapper.likeLeft("name", "五");
-
-        List<User> users = this.userMapper.selectList(wrapper);
-        for (User user : users) {
-            System.out.println(user);
-        }
+        userMapper.selectList(wrapper);
     }
 
     @Test
-    public void testOrderByAgeDesc(){
+    public void testOrderByAgeDesc() {
         QueryWrapper<User> wrapper = new QueryWrapper<>();
         //按照年龄倒序排序
         // SELECT id,user_name,name,age,email AS mail FROM tb_user ORDER BY age DESC
         wrapper.orderByDesc("age");
+        userMapper.selectList(wrapper);
 
-        List<User> users = this.userMapper.selectList(wrapper);
-        for (User user : users) {
-            System.out.println(user);
-        }
     }
 
     @Test
-    public void testOr(){
+    public void testOr() {
         QueryWrapper<User> wrapper = new QueryWrapper<>();
         // SELECT id,user_name,name,age,email AS mail FROM tb_user WHERE name = ? OR age = ?
         wrapper.eq("name", "王五").or().eq("age", 21);
@@ -332,20 +266,15 @@ class MybatisPlusDemoApplicationTests {
     }
 
     @Test
-    public void testSelect(){
+    public void testSelect() {
         QueryWrapper<User> wrapper = new QueryWrapper<>();
         //SELECT id,name,age FROM tb_user WHERE name = ? OR age = ?
         wrapper.eq("name", "王五")
                 .or()
                 .eq("age", 21)
-                .select("id","name","age"); //指定查询的字段
-
-        List<User> users = this.userMapper.selectList(wrapper);
-        for (User user : users) {
-            System.out.println(user);
-        }
+                .select("id", "name", "age"); //指定查询的字段
+        userMapper.selectList(wrapper);
     }
-
 
 
 }
